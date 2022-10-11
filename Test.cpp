aca23428701ec9c246ea8ff0ceca6c5d32d74c3b
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-10-11 16:33:21 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a;
+	int b;
+	int result;
+
+	std::cout << "Input number 1: ";
+	std::cin >> a;
+
+	std::cout << "Input number 2: ";
+	std::cin >> b;
+
+	result = a + b;
+	std::cout << result;
+
+	system("pause");
+}
